7965c049d1279e6b0583f0f778798a88ab0e84bf
fentontaylor <fentontaylor@gmail.com>
2020-01-12 15:52:31 -0700

    Add test for cascading delete to event.spec.js

--- tests/models/event.spec.js
+++ tests/models/event.spec.js
@@ -2,15 +2,15 @@ const config = require('../../knexfile')['test'];
 const DB = require('knex')(config);
 
 describe('Olympics table', () => {
-  beforeEach(async () => {
+  beforeEach(async() => {
     await DB.raw("TRUNCATE TABLE sports CASCADE");
   });
 
-  afterEach(async () => {
+  afterEach(async() => {
     await DB.raw("TRUNCATE TABLE sports CASCADE");
   });
 
-  it('has an id and name', async () => {
+  it('has an id and name', async() => {
     await DB('sports')
       .insert({ 'id': 1, 'sport': 'Weightlifting' })
     
@@ -18,7 +18,8 @@ describe('Olympics table', () => {
       .insert({
         'id': 1,
         'sport_id': 1,
-        'event': "Weightlifting Women's Super- Heavyweight" })
+        'event': "Weightlifting Women's Super- Heavyweight"
+      })
 
     let event = await DB('sports')
       .join('events', 'sports.id', '=', 'events.sport_id')
@@ -26,5 +27,28 @@ describe('Olympics table', () => {
     expect(event[0].sport).toBe('Weightlifting')
     expect(event[0].sport_id).toBe(1)
     expect(event[0].event).toBe("Weightlifting Women's Super- Heavyweight")
+  })
+
+  it('cascades on delete', async() => {
+    await DB('sports')
+      .insert({ 'id': 1, 'sport': 'Weightlifting' })
+
+    await DB('events')
+      .insert({
+        'id': 1,
+        'sport_id': 1,
+        'event': "Weightlifting Women's Super- Heavyweight"
+      })
+
+    var sports = await DB('sports')
+    var events = await DB('events')
+    expect(sports.length).toBe(1)
+    expect(events.length).toBe(1)
+
+    await DB('sports').del()
+    var sports = await DB('sports')
+    var events = await DB('events')
+    expect(sports.length).toBe(0)
+    expect(events.length).toBe(0)
   })
 })
