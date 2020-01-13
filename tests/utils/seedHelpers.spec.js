9cc49cf1d4214c82e0f408f8503d538b3d12a9d6
fentontaylor <fentontaylor@gmail.com>
2020-01-12 20:53:17 -0700

    Add test for createAthlete with full data

--- tests/utils/seedHelpers.spec.js
+++ tests/utils/seedHelpers.spec.js
@@ -47,5 +47,30 @@ describe('Seed Helper functions', () => {
       let allOlympics = await DB('olympics');
       expect(allOlympics.length).toBe(1);
     })
+
+    describe('createAthlete', () => {
+      it('creates an athlete record from data', async () => {
+        let data = {
+          Name: 'Ciara Everard',
+          Sex: 'F',
+          Age: '26',
+          Height: '169',
+          Weight: '54',
+          Team: 'Ireland',
+          Games: '2016 Summer',
+          Sport: 'Athletics',
+          Event: 'Athletics Women\'s 800 metres',
+          Medal: 'NA'
+        }
+
+        let athlete = await createAthlete(data);
+        expect(athlete.name).toBe('Ciara Everard');
+        expect(athlete.sex).toBe('F');
+        expect(athlete.height).toBe(169);
+        expect(athlete.weight).toBe(54);
+        expect(athlete.age).toBe(26);
+        expect(athlete.team).toBe('Ireland');
+      })
+    })
   })
 })
