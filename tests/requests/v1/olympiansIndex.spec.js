8dafab361093b3a164278a6106a872116b6c5ac7
fentontaylor <fentontaylor@gmail.com>
2020-01-13 15:28:41 -0700

    Add basic test for olympians index

---
+++ tests/requests/v1/olympiansIndex.spec.js
@@ -0,0 +1,49 @@
+var request = require("supertest");
+var app = require('../../../app');
+
+const config = require('../../../knexfile')['test'];
+const DB = require('knex')(config);
+const { dbSetupWith } = require('../../../utils/dbSeed');
+const {
+  createOlympics,
+  createAthlete,
+  createSport,
+  createEvent,
+  createAthleteEvent,
+  destroyAll
+} = require('../../../utils/dbHelpers');
+
+
+describe('GET /api/v1/olympians', ()=>{
+  beforeEach(async () => {
+    await destroyAll();
+    data = {
+      Name: 'Ciara Everard',
+      Sex: 'F',
+      Age: '26',
+      Height: '169',
+      Weight: '54',
+      Team: 'Ireland',
+      Games: '2016 Summer',
+      Sport: 'Athletics',
+      Event: 'Athletics Women\'s 800 metres',
+      Medal: 'Bronze'
+    }
+  });
+
+  afterEach(async () => {
+    await destroyAll();
+  });
+
+  it('example', async () => {
+    let athlete = await createAthlete(data);
+    let olympics = await createOlympics(data);
+    let sport = await createSport(data);
+    let event = await createEvent(data, sport);
+    let athleteEvent = await createAthleteEvent(data, athlete, event, olympics);
+    
+    var response = await request(app)
+      .get('/api/v1/olympians')
+    expect(response.status).toBe(200)
+  })
+})
