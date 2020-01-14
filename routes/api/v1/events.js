77052130aa4f5dd0669ecf3bb13234c506a34b0c
fentontaylor <fentontaylor@gmail.com>
2020-01-13 19:55:31 -0700

    Update events Router to call sportEvents

--- routes/api/v1/events.js
+++ routes/api/v1/events.js
@@ -2,10 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 const DB = require('../../../utils/dbConnect');
-// const { olympianIndex } = require('../../../utils/dbQueries');
+const { sportEvents } = require('../../../utils/dbQueries');
 
 router.get('/', (request, response) => {
-  response.status(200).send({message: 'hi'})
+  sportEvents()
+    .then((result) => response.status(200).send(result))
+    .catch((error) => response.status(500).send({ error }))
 });
 
 module.exports = router;
