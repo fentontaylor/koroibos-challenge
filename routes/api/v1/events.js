18dc4f7dbc99d3d37941496163711229ab6c0754
fentontaylor <fentontaylor@gmail.com>
2020-01-13 19:52:40 -0700

    Add basic logic for in events Router

---
+++ routes/api/v1/events.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+const DB = require('../../../utils/dbConnect');
+// const { olympianIndex } = require('../../../utils/dbQueries');
+
+router.get('/', (request, response) => {
+  response.status(200).send({message: 'hi'})
+});
+
+module.exports = router;
