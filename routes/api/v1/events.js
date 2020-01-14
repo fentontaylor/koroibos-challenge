c5f804f82ad31e09fc6f94d986b19eee3b556fd9
fentontaylor <fentontaylor@gmail.com>
2020-01-13 23:00:34 -0700

    Add .id to request params for eventId

--- routes/api/v1/events.js
+++ routes/api/v1/events.js
@@ -12,7 +12,7 @@ router.get('/', (request, response) => {
 });
 
 router.get('/:id/medalists', (request, response) => {
-  let eventId = parseInt(request.params)
+  let eventId = parseInt(request.params.id)
   eventMedalists(eventId)
     .then((result) => response.status(200).send(result))
     .catch((error) => response.status(500).send(error))
