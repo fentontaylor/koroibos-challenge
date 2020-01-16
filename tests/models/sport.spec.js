c22948ac42b756c0dd7ab8ea71a5eaa11247f463
fentontaylor <fentontaylor@gmail.com>
2020-01-15 18:27:56 -0700

    Add test for Sport model findOrCreate

---
+++ tests/models/sport.spec.js
@@ -0,0 +1,48 @@
+const Sport = require('../../models/sports')
+const { destroyAll } = require('../../utils/dbHelpers')
+const DB = require('../../utils/dbConnect')
+
+describe('Sport Model', () => {
+  beforeEach(async () => {
+    await destroyAll();
+  })
+
+  afterEach(async () => {
+    await destroyAll();
+  })
+
+  it('has a tableName', () => {
+    expect(Sports.tableName).toBe('sports')
+  })
+
+  it('can findOrCreate from a row of data', async () => {
+    let data = {
+      name: 'Katniss Everdeen',
+      team: 'USA',
+      sex: 'F',
+      height: 160,
+      weight: 50,
+      age: 19,
+      games: '2016 Summer',
+      sport: 'Archery',
+      event: 'Archery Women\'s Individual',
+      medal: 'Gold'
+    }
+
+    // This should create the sports record since it does not exist
+    let sports = await Sport.findOrCreate(data);
+
+    expect(sports.games).toBe(data.games);
+
+    let result = await DB('sports');
+    expect(sports).toEqual(result[0]);
+    expect(result.length).toBe(1);
+
+    // This should find the athlete since she already exists
+    let olympics2 = await Sport.findOrCreate(data);
+
+    let result2 = await DB('sports');
+    expect(olympics2).toEqual(result2[0]);
+    expect(result2.length).toBe(1);
+  })
+})
