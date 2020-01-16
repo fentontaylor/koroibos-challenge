596fe8c03b83499877e8e1c70297b73290cbf737
fentontaylor <fentontaylor@gmail.com>
2020-01-15 18:25:34 -0700

    Add logic for Olympics.findOrCreate() to model

---
+++ models/olympics.js
@@ -0,0 +1,20 @@
+const { Model } = require('objection');
+const DB = require('../utils/dbConnect');
+Model.knex(DB);
+
+class Olympics extends Model {
+  static get tableName() {
+    return 'olympics'
+  }
+
+  static async findOrCreate(data) {
+    let gamesData = { games: data.games };
+    let result = await this.query().where(gamesData);
+    if (result.length === 0) {
+      return await this.query().insertAndFetch(gamesData);
+    }
+    return result[0];
+  }
+}
+
+module.exports = Olympics;
